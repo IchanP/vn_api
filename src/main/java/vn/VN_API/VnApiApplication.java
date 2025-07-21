bbc4eea56a1291d26009de647f53bfc564ebb837
Pontus <smeaurix@gmail.com>
2025-07-21 17:53:33 +0200

    init

---
+++ src/main/java/vn/VN_API/VnApiApplication.java
@@ -0,0 +1,13 @@
+package vn.VN_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnApiApplication.class, args);
+	}
+
+}
