0944e89759875e154ec698d624f31f9d9c78a1dc
Pontus <smeaurix@gmail.com>
2025-08-04 22:41:26 +0200

    add a vntitle batchloader

---
+++ src/main/java/vn/VN_API/graphql/dataloader/VnTitlesBatchLoader.java
@@ -0,0 +1,29 @@
+package vn.VN_API.graphql.dataloader;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.stream.Collectors;
+import org.dataloader.BatchLoader;
+import vn.VN_API.entity.VnTitlesEntity;
+import vn.VN_API.repository.VisualNovelTitleRepository;
+
+public class VnTitlesBatchLoader implements BatchLoader<String, List<VnTitlesEntity>> {
+
+  private final VisualNovelTitleRepository vnTitleRepository;
+
+  public VnTitlesBatchLoader(VisualNovelTitleRepository repository) {
+    this.vnTitleRepository = repository;
+  }
+
+  // TODO write a test for this function!
+  @Override
+  public CompletionStage<List<List<VnTitlesEntity>>> load(List<String> vnIds) {
+    List<VnTitlesEntity> allTitles = this.vnTitleRepository.findByIdVnIdIn(vnIds);
+
+    return CompletableFuture.supplyAsync(() -> vnIds
+        .stream().map(vnId -> allTitles.stream()
+            .filter(title -> title.getId().getVnId().equals(vnId)).collect(Collectors.toList()))
+        .collect(Collectors.toList()));
+  }
+}
